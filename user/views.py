4b2926ba9fb7e61680ed891d05fd241297da5c79
Szymon Sciegienny <szymon.m.sciegienny@gmail.com>
2022-03-23 18:59:50 +0100

    Registration and User Detail view work

--- user/views.py
+++ user/views.py
@@ -1,23 +1,19 @@
 import imp
 from django.shortcuts import redirect, render
-from django.contrib import messages
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegistrationForm, UserUpdateForm 
 
 def register(request):
-    if request == "POST":
+    if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save()
-            username = user.cleaned_data.get('username')
-            messages.success(request, f"New Account: {username}")
             login(request, user)
-            messages.info(request, f"Logged In as: {username}")
             return redirect("video:video-list")
     else:
         form = UserRegistrationForm()
-    return render(request, "user/register.html", {"form":form})
+    return render(request, "user/register.html", {"form": form})
 
 
 @login_required
@@ -26,7 +22,6 @@ def user(request):
         updated_form = UserUpdateForm(request.POST, instance=request.user)
         if updated_form.is_valid():
             updated_form.save()
-            messages.success(request, f"Your account has been updated!")
             return redirect("user")
     else:
         updated_form = UserUpdateForm(instance=request.user)
